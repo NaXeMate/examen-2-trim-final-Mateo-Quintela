29f2946e2c53d7f5734a83ff816186e56b333a22
NaXeMate <181981244+NaXeMate@users.noreply.github.com>
2026-02-20 08:52:44 +0100

    Initial commit

---
+++ src/main/java/edu/examen/misionEstelar/MisionEstelarApplication.java
@@ -0,0 +1,13 @@
+package edu.examen.misionEstelar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisionEstelarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisionEstelarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/examen/misionEstelar/MisionEstelarApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.examen.misionEstelar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisionEstelarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
